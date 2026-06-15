baadc0e8ad5b4dc727888eb2733143f0fc932ea6
it26101402 <it26101402@my.sliit.lk>
2026-06-15 18:20:33 +0530

    Added answer of Q2

---
+++ Q2/IT26101402Lab2Q2.java
@@ -0,0 +1,8 @@
+public class IT26101402Lab2Q2 {
+	public static void main(String [] args){ 
+		
+		double Sidelen = 10;
+		double Radius = Sidelen*7/11;
+		System.out.println("Radius of the circle is "+Radius);
+	}
+} 
