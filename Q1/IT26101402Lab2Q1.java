a5acdb7b81646e9762ae4a91ae0acf0908f8e095
it26101402 <it26101402@my.sliit.lk>
2026-06-15 18:19:45 +0530

    Added answer of Q1

---
+++ Q1/IT26101402Lab2Q1.java
@@ -0,0 +1,10 @@
+public class IT26101402Lab2Q1 {
+	public static void main(String[] args){ 
+		double perimeter = 100;
+		double length = perimeter / (2 * (1+3.0/4));
+		double width = (3.0/4) * length;
+		System.out.println("Length of the fernce: "+length);
+		System.out.println("Width of  the fence:"+width);
+	} 
+
+}
